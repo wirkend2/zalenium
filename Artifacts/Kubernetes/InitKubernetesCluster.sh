88906adcbf2332ce9200dc43e3a78902689fbffe
wirkend2 <49435295+wirkend2@users.noreply.github.com>
2019-06-24 14:41:55 +0200

    Remove sudo

--- Artifacts/Kubernetes/InitKubernetesCluster.sh
+++ Artifacts/Kubernetes/InitKubernetesCluster.sh
@@ -1,8 +1,5 @@
 #Install kubernets on Ubuntu 16.04
 
-#Get the input parameter value.
-LINUX_USER=$1
-
 #Docker install
 #sudo apt install docker.io
 #Docker upgrade
@@ -10,14 +7,14 @@ LINUX_USER=$1
 
 #Kubernetes install
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-sudo apt install kubeadm -y
-sudo swapoff -a
+apt-get update
+apt install kubeadm -y
+swapoff -a
 
 #Init kubeadm --> Docker version could be to low in case of no upgrade of base image
-sudo kubeadm init --ignore-preflight-errors=SystemVerification
+kubeadm init --ignore-preflight-errors=SystemVerification
 
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
@@ -26,26 +23,26 @@ chown $(id -u):$(id -g) /root/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 #Deploy pod weave network
-sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(sudo kubectl version | base64 | tr -d '\n')"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 #Allow pod scheduling on master node (non-production)
-sudo kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
 #FileShare
-sudo mkdir /mnt/adevtestlabaks193
+mkdir /mnt/adevtestlabaks193
 if [ ! -d "/etc/smbcredentials" ]; then
-sudo mkdir /etc/smbcredentials
+mkdir /etc/smbcredentials
 fi
 if [ ! -f "/etc/smbcredentials/adevtestlabaks193.cred" ]; then
-    sudo bash -c 'echo "username=adevtestlabaks193" >> /etc/smbcredentials/adevtestlabaks193.cred'
-    sudo bash -c 'echo "password=W8iUebsZtBp3mgcKd62rJIDG3SmnCKlMyRZS7nxe+4YXWJU2D2DDyCxXOyBWoh7xBqLan6WxqY7yQY6d5OmrVQ==" >> /etc/smbcredentials/adevtestlabaks193.cred'
+    bash -c 'echo "username=adevtestlabaks193" >> /etc/smbcredentials/adevtestlabaks193.cred'
+    bash -c 'echo "password=W8iUebsZtBp3mgcKd62rJIDG3SmnCKlMyRZS7nxe+4YXWJU2D2DDyCxXOyBWoh7xBqLan6WxqY7yQY6d5OmrVQ==" >> /etc/smbcredentials/adevtestlabaks193.cred'
 fi
-sudo chmod 600 /etc/smbcredentials/adevtestlabaks193.cred
+chmod 600 /etc/smbcredentials/adevtestlabaks193.cred
 
-sudo bash -c 'echo "//adevtestlabaks193.file.core.windows.net/templates /mnt/adevtestlabaks193 cifs nofail,vers=3.0,credentials=/etc/smbcredentials/adevtestlabaks193.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
-sudo mount -t cifs //adevtestlabaks193.file.core.windows.net/templates /mnt/adevtestlabaks193 -o vers=3.0,credentials=/etc/smbcredentials/adevtestlabaks193.cred,dir_mode=0777,file_mode=0777,serverino
+bash -c 'echo "//adevtestlabaks193.file.core.windows.net/templates /mnt/adevtestlabaks193 cifs nofail,vers=3.0,credentials=/etc/smbcredentials/adevtestlabaks193.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+mount -t cifs //adevtestlabaks193.file.core.windows.net/templates /mnt/adevtestlabaks193 -o vers=3.0,credentials=/etc/smbcredentials/adevtestlabaks193.cred,dir_mode=0777,file_mode=0777,serverino
 
 #Create namespace first
-sudo kubectl apply -f /mnt/adevtestlabaks193/ns.yml
+kubectl apply -f /mnt/adevtestlabaks193/ns.yml
 
-sudo kubectl apply -f /mnt/adevtestlabaks193
+kubectl apply -f /mnt/adevtestlabaks193
